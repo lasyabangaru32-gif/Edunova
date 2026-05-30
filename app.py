50e67752b82143f0faf675a6fba3b92c6790e63e
Lasya <lasyabangaru32@gmail.com>
2026-05-30 17:38:33 +0530

    Fix dashboard missing CSS

--- app.py
+++ app.py
@@ -349,4 +349,4 @@ def help():
 
 # ---------------- RUN APP ----------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=10000, debug=True) 
